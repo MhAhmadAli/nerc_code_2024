3c482f6dfda1ee44f6037a2015ba68f1a65a344d
MhAhmadAli <fa17-bsse-023@lgu.edu.pk>
2024-07-09 08:26:06 +0500

    RED: diagonal done

--- src/main.cpp
+++ src/main.cpp
@@ -528,20 +528,25 @@ void red()
   backwardWithEncoder(50, 150, 200);
   delay(1000);
   // move servo down for pickup
-  frontServoMove(95, 0);
+  // frontServoMove(95, 0);
+  frontServo.write(0);
+
   delay(1000);
+
+  // ============= first tree and rock =============
   // move until first tree
   linefollowEncoder(280);
   delay(1000);
   // move servo up after pickup
-  frontServoMove(0, 45);
+  frontServoMove(0, 90);
   delay(1000);
   // move forward for 1 strip
   linefollowUntil(1);
   delay(1000);
   // turn right for rock
   right90(83);
-  backServoMove(95, 0);
+    backServo.write(0);
+  // backServoMove(95, 0);
   delay(1000);
   // move back 3 strips
   backwardWithEncoder(50, 60);
@@ -550,18 +555,18 @@ void red()
   //move to pick up rock
   backLinefollowEncoder(100);
   delay(1000);
-  backServoMove(0, 45);
+  backServoMove(0, 90);
   delay(1000);
   // turn 180 after rock pick up
   rightTurnEncoder(60, (240 * 2) + 5);
   delay(1000);
   // adjust to line for tree drop
-  backLinefollowEncoder(200);
+  backLinefollowUntil(2);
   delay(500);
-  linefollowEncoder(200);
+  linefollowEncoder(400);
   delay(1000);
   // drop tree
-  frontServoMove(45, 0);
+  frontServoMove(90, 0);
   delay(1000);
   // move back 3 strip to drop rock at original tree position
   backLinefollowUntil(3);
@@ -569,8 +574,11 @@ void red()
   backLinefollowEncoder(90);
   delay(1000);
   // drop rock
-  backServoMove(45, 0);
+  backServoMove(90, 0);
   delay(1000);
+  // ============= first tree and rock =============
+
+  // ============= second tree and rock =============
   // move back to intersection after rock drop
   linefollowUntil(1);
   delay(500);
@@ -590,8 +598,116 @@ void red()
   linefollowEncoder(200);
   delay(500);
   // move front servo up after tree pickup
-  frontServoMove(0, 45);
+  frontServoMove(0, 90);
   delay(500);
+  // move forward to intersection
+  linefollowUntil(1);
+  delay(1000);
+  // turn right for rock
+  right90(80);
+  delay(1000);
+  // move back 2 strips
+  backwardWithEncoder(50, 80);
+  backLinefollowUntil(2);
+  delay(1000);
+  //move to pick up rock
+  backLinefollowEncoder(100);
+  delay(1000);
+  backServoMove(0, 90);
+  delay(1000);
+  // turn 180 after rock pick up
+  rightTurnEncoder(60, (240 * 2) + 5);
+  delay(1000);
+  // adjust to line for tree drop
+  backLinefollowUntil(2);
+  delay(500);
+  linefollowEncoder(400);
+  delay(1000);
+  // drop tree
+  frontServoMove(90, 0);
+  delay(1000);
+  // move back 2 strip to drop rock at original tree position
+  backLinefollowUntil(2);
+  delay(1000);
+  backLinefollowEncoder(90);
+  delay(1000);
+  // drop rock
+  backServoMove(90, 0);
+  delay(1000);
+  // ============= second tree and rock =============
+
+  // ============= third tree and rock =============
+  // move back to intersection after rock drop
+  linefollowUntil(1);
+  delay(500);
+  // move front servo up for the turn
+  frontServoMove(0, 95);
+  delay(500);
+  // turn right
+  right90(80);
+  delay(1000);
+  // move forward away from tree to adjust for line
+  backLinefollowEncoder(100);
+  delay(1000);
+  // move front servo down for tree pickup
+  frontServoMove(95, 0);
+  delay(500);
+  // move forward to tree
+  linefollowEncoder(200);
+  delay(500);
+  // move front servo up after tree pickup
+  frontServoMove(0, 90);
+  delay(500);
+  // move forward to intersection
+  linefollowUntil(1);
+  delay(1000);
+  // turn right for rock
+  right90(80);
+  delay(1000);
+  // move back 1 strips
+  backwardWithEncoder(50, 80);
+  backLinefollowUntil(1);
+  delay(1000);
+  //move to pick up rock
+  backLinefollowEncoder(100);
+  delay(1000);
+  backServoMove(0, 90);
+  delay(1000);
+  // turn 180 after rock pick up
+  rightTurnEncoder(60, (240 * 2) + 5);
+  delay(1000);
+  // adjust to line for tree drop
+  backLinefollowUntil(2);
+  delay(500);
+  linefollowEncoder(400);
+  delay(1000);
+  // drop tree
+  frontServoMove(90, 0);
+  delay(1000);
+  // move back 1 strip to drop rock at original tree position
+  backLinefollowUntil(1);
+  delay(1000);
+  backLinefollowEncoder(90);
+  delay(1000);
+  // drop rock
+  backServoMove(90, 0);
+  delay(1000);
+
+/// waqas code
+frontServo.write(100);
+  delay(1000);
+   linefollowUntil(1);
+  linefollowEncoder(10);
+
+   delay(1000);
+     left90(80);
+     delay(1000);
+     linefollowEncoder(60);
+     linefollowUntil(4);
+     linefollowEncoder(150);
+   delay(1000);
+
+  // ============= third tree and rock =============
   while (1)
   {
     Serial.println("END");
